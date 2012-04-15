4af908e408901ed5f1dcacaf18875815a6983a4b
Conor <fennell.conor@gmail.com>
2012-04-15 17:11:44 +0100

    Incomplete - Geting login to run as asynchronous task. AndroidManifest.xml - Added new login activity pom.xml - added dependancy for apache http commons LoginTask.java - task for logging in using apache http commons, still 				have to handle cookie coming back after first POST login.xml - pretty much a copy of the main.xml Brian done up. I think we 			should have a layout xml file per activity and name them the same LoginActivity.java - This is set up to launch the asynchronous login

--- src/main/java/com/showrss/LoginTask.java
+++ src/main/java/com/showrss/LoginTask.java
@@ -3,19 +3,25 @@ package com.showrss;
 //TODO: Fix imports
 import java.io.*;
 
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.util.Log;
 
-public class LoginTask implements Runnable{
+public class LoginTask implements Runnable {
 	private static final String TAG = "LoginTask";
 	private final String userName, password;
 
-	
-	
 	public LoginTask(String userName, String password) {
 		this.userName = userName;
 		this.password = password;
@@ -24,64 +30,44 @@ public class LoginTask implements Runnable{
 	@Override
 	public void run() {
 		attemptLogin(userName, password);
-		
+
 	}
-	
-	private boolean attemptLogin (String user, String pass)
-	{
-		Log.d(TAG, "Attempting to login as: " + user );
-		
-		
-		//Should we throw and exception on failed logins?
-		if (!validateUserName(user))
-		{
-			Log.d(TAG, "Invalid Login name" );
+
+	private boolean attemptLogin(String user, String pass) {
+		Log.d(TAG, "Attempting to login as: " + user);
+
+		// Should we throw and exception on failed logins?
+		if (!validateUserName(user)) {
+			Log.d(TAG, "Invalid Login name");
 			return false;
 		}
-		
-		//TODO: Get this working
-		//String loginURL = getString(R.string.loginURL);
+
+		// TODO: Get this working
+		// String loginURL = getString(R.string.loginURL);
+
 		String loginURL = "http://showrss.karmorra.info/?cs=login";
-		
+
 		try {
-			
-			String charset = "UTF-8";
-			
-			URL url = new URL(loginURL);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			
-			//TODO: Do we need to set these? Are they right?
-			con.setReadTimeout(10000 /*Milliseconds*/);
-			con.setConnectTimeout(15000 /*Milliseconds*/);
-			
-			con.setRequestMethod("POST");
-			
-			con.setRequestProperty("ContentType", "application/x-www-form-urlencoded");
-			con.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-			con.setRequestProperty("Accept-Charset", charset);
-			con.setRequestProperty("Accept-Encoding", "text/html,application/xhtml+xml,application/xml");
-			con.setRequestProperty("Content-Length", "32");
-			
-			String query = "username=" + URLEncoder.encode(user, charset);
-			query = query + "&password=" + URLEncoder.encode(password, "UTF-8");
-			
-			con.setDoOutput(true);
-			
-			OutputStream output = null;
-			try {
-			     output = con.getOutputStream();
-			     output.write(query.getBytes(charset));
-			} finally {
-			     if (output != null) try { output.close(); } catch (IOException logOrIgnore) {}
-			}
-			
-			InputStream response = con.getInputStream();
-			
-			
-			
-			//TODO: Figure out how to read the response that comes back!
-			
-			
+
+			// Create a new HttpClient and Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(loginURL);
+
+			// Add your data
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+			nameValuePairs.add(new BasicNameValuePair("username", this.userName));
+			nameValuePairs.add(new BasicNameValuePair("password", this.password));
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+			// Execute HTTP Post Request and get response
+			HttpResponse response = httpclient.execute(httppost);
+
+			Header[] y = response.getAllHeaders();
+
+			System.out.println(y);
+
+			// TODO: Figure out how to read the response that comes back!
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -91,12 +77,12 @@ public class LoginTask implements Runnable{
 		}
 		return true;
 	}
-	
 
-	//ShowRSS rules on valid user names:
-	//"The username should be a word without strange symbols, between 2 and 12 characters, and not be in use by another user."
-	private boolean validateUserName(String user){
-		//"between 2 and 12" sounds like 3-11 but im airing on the side of caution sayings its 2-12
+	// ShowRSS rules on valid user names:
+	// "The username should be a word without strange symbols, between 2 and 12 characters, and not be in use by another user."
+	private boolean validateUserName(String user) {
+		// "between 2 and 12" sounds like 3-11 but im airing on the side of
+		// caution sayings its 2-12
 		return (user.length() >= 2 && user.length() <= 12);
 	}
 
---
+++ src/main/java/com/showrss/activitys/LoginActivity.java
@@ -0,0 +1,91 @@
+package com.showrss.activitys;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.showrss.LoginTask;
+import com.showrss.R;
+
+public class LoginActivity extends Activity {
+
+	// Declarations for threading
+	private Handler guiThread;
+	private ExecutorService transThread;
+	@SuppressWarnings("rawtypes")
+	private Future transPending;
+	private Runnable loginTask;
+	
+	private String username;
+	private String password;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		
+		
+		
+		this.initThreading();
+	}
+
+	private void setupViews(){
+		this.findViewById(R.id.loginButton);
+	}
+	
+
+	/**
+	 * Request the task to start after a short delay
+	 * 
+	 * @param task
+	 * @param delayMillis
+	 */
+	private void queueUpdate(Runnable task, long delayMillis) {
+		// Cancel previous update if it hasn't started yet
+		this.guiThread.removeCallbacks(task);
+		// Start an update if nothing happens after a few milliseconds
+		this.guiThread.postDelayed(task, delayMillis);
+	}
+	
+	
+
+	/**
+	 * Sets up the login task so it can be ran
+	 * 
+	 */
+	private void initThreading() {
+		this.guiThread = new Handler();
+		this.transThread = Executors.newSingleThreadExecutor();
+
+		this.loginTask = new Runnable() {
+			@Override
+			public void run() {
+
+				// Cancel previous login task
+				if (LoginActivity.this.transPending != null) {
+					LoginActivity.this.transPending.cancel(true);
+				}
+
+				// TODO:Let user know we're doing something
+
+				// Begin the login request but don't wait for it
+
+				try {
+					
+					LoginTask loginTask = new LoginTask(LoginActivity.this.username, LoginActivity.this.password);
+					LoginActivity.this.transPending = LoginActivity.this.transThread.submit(loginTask);
+				} catch (RejectedExecutionException e) {
+					// Unable to start new task
+					//TODO: ADD Error
+				}
+
+			}
+		};
+	}
+
+}
