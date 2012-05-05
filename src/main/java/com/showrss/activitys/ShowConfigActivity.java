7e2d366ffe8fee374297e7028fcc893a8e424ba0
igiggles <gjglc2@gmail.com>
2012-05-05 18:05:27 +0100

    Added functionality to save changes to that show. It kind of works, but seems to have stopped saving. Not sure why. Will look at it again Monday.

--- src/main/java/com/showrss/activitys/ShowConfigActivity.java
+++ src/main/java/com/showrss/activitys/ShowConfigActivity.java
@@ -1,17 +1,31 @@
 package com.showrss.activitys;
 
 import com.showrss.R;
+import com.showrss.YourShows;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ShowConfigActivity extends Activity
+public class ShowConfigActivity extends Activity implements OnItemSelectedListener 
 {
 	String showName = "";
+	
 	Button button;
+	CheckBox box;
+	Spinner spinner;
+	
+	boolean inHD = false;
+	boolean isBoth = false;
+	boolean inSD = false;
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
@@ -28,13 +42,68 @@ public class ShowConfigActivity extends Activity
 		    public void onClick(View v) 
 		    {
 		    	saveSettings();
-		    	finish();
 		    }
 		  });
+		
+		spinner = (Spinner) findViewById(R.id.spinner1);
+		spinner.setOnItemSelectedListener(this);
+		
+		box = (CheckBox) findViewById(R.id.checkBox1);
 	}
 	
 	private void saveSettings()
 	{
-		//String showId = AllShows.showNameAsKey.get(showName);
+		if(inHD || inSD || isBoth)
+		{
+			if(isBoth)
+			{
+				YourShows.showSettings(showName, true, true, box.isChecked());
+			}
+			else
+			{
+				YourShows.showSettings(showName, inSD, inHD, box.isChecked());
+			}			
+		}
+		else
+		{
+			Toast.makeText(this, "You need to select a quality value.", Toast.LENGTH_SHORT).show();
+		}
+		
+		finish();
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) 
+	{
+		String selected = parent.getItemAtPosition(pos).toString();
+		if(selected.equals("HD"))
+		{
+			inSD = false;
+			inHD = true;
+			isBoth = false;
+		}
+		else if(selected.equals("SD"))
+		{
+			inSD = true;
+			inHD = false;
+			isBoth = false;
+		}
+		else if(selected.equals("Both"))
+		{
+			inSD = false;
+			inHD = false;
+			isBoth = true;
+		}
+	}
+
+	/**
+	 * If nothing is selected, we have to reset everything back
+	 */
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) 
+	{
+		inHD = false;
+		inSD = false;
+		isBoth = false;
 	}
 }
